50fb8e91ddcff88ec2f7499a502d533c7c279c2c
oudaming <805269294@qq.com>
2021-02-03 23:39:39 +0800

    编辑字符串花最小代价问题

--- src/class05/Code02_EditCost.java
+++ src/class05/Code02_EditCost.java
@@ -1,7 +1,19 @@
 package class05;
 
+/**
+ * 编辑字符串花最小代价问题
+ */
 public class Code02_EditCost {
 
+	/**
+	 * O(N * M)
+	 * @param s1 原字符串
+	 * @param s2 新字符串
+	 * @param ic 插入代价
+	 * @param dc 删除代价
+	 * @param rc 替换代价
+	 * @return
+	 */
 	public static int minCost1(String s1, String s2, int ic, int dc, int rc) {
 		if (s1 == null || s2 == null) {
 			return 0;
@@ -10,22 +22,33 @@ public class Code02_EditCost {
 		char[] str2 = s2.toCharArray();
 		int N = str1.length + 1;
 		int M = str2.length + 1;
+		// dp[i][j] 表示长度为i的str1变成长度为j的str2的代价，所以N 是length + 1
 		int[][] dp = new int[N][M];
-		// dp[0][0]  = 0
+		// 零行零列表示变成空字符串时的代价
+		// dp[0][0]  = 0  空字符串变成空字符串的代价是copy代价，成本定义为0
 		for (int i = 1; i < N; i++) {
+			// 将长度为i的字符串变成空字符串，肯定是i个删除代价
 			dp[i][0] = dc * i;
 		}
 		for (int j = 1; j < M; j++) {
+			// 将空字符串变成长度为j的字符串，肯定是j个添加代价
 			dp[0][j] = ic * j;
+
 		}
+		// 例如： abc -> acf, 前面填好了第0行和第0列，现在从第一行开始填
 		for (int i = 1; i < N; i++) {
 			for (int j = 1; j < M; j++) {
+				// 如果 a == a,那么就是copy,没有代价
 				if (str1[i - 1] == str2[j - 1]) {
 					dp[i][j] = dp[i - 1][j - 1];
 				} else {
+					// 如果是b -> c， 就是替换的代价
 					dp[i][j] = dp[i - 1][j - 1] + rc;
 				}
+				// 如果i = 2 && j =2 ,那么目的就是将 ab -> ac
+				// ab -> a  然后 再在a后添加一个c,就是一个添加的代价
 				dp[i][j] = Math.min(dp[i][j], dp[i][j - 1] + ic);
+				// a -> ac   然后那么原来的ab就变成acb,这样需要删除b,就是需要一个删除的代价， min都是求最小代价
 				dp[i][j] = Math.min(dp[i][j], dp[i - 1][j] + dc);
 			}
 		}

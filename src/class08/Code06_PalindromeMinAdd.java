7b818255de25ef8bf5011e7f53a0ddf215f021ed
oudaming <805269294@qq.com>
2021-02-18 22:05:09 +0800

    可以在字符串的任意位置添加字符串，问要使得整个字符串是回文，最少要添加几个字符

--- src/class08/Code06_PalindromeMinAdd.java
+++ src/class08/Code06_PalindromeMinAdd.java
@@ -1,7 +1,15 @@
 package class08;
 
+/**
+ * 可以在字符串的任意位置添加字符串，问要使得整个字符串是回文，最少要添加几个字符？
+ */
 public class Code06_PalindromeMinAdd {
 
+	/**
+	 * 还原得到最后的串是什么
+	 * @param str
+	 * @return
+	 */
 	public static String getPalindrome1(String str) {
 		if (str == null || str.length() < 2) {
 			return str;
@@ -28,9 +36,17 @@ public class Code06_PalindromeMinAdd {
 		return String.valueOf(res);
 	}
 
+	/**
+	 * 动态规划找到需要添加串的数量
+	 * @param str
+	 * @return
+	 */
 	public static int[][] getDP(char[] str) {
+		// dp[i][j] 表示要使得[i,j]是回文，要至少添加几个字符
 		int[][] dp = new int[str.length][str.length];
+		// 对角线 i==j，字符本身就是回文，需要添加0个字符
 		for (int j = 1; j < str.length; j++) {
+			// 长度为2的字符，如果两相等就是0，不相等添加一个即可
 			dp[j - 1][j] = str[j - 1] == str[j] ? 0 : 1;
 			for (int i = j - 2; i > -1; i--) {
 				if (str[i] == str[j]) {
